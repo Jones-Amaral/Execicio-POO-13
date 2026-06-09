83277a5f32fbafd126ec50f8942aae836fe684cc
João Vitor Alves Amaral <am.joao13@gmail.com>
2026-06-09 08:49:04 -0300

    Exercicio 1 concluído

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ class Carro : Veiculo
     public string tipoCombustivel { get; set; }
     public override void ExibirDetalhes()
     {
-        System.Console.WriteLine($"Modelo: {this.modelo} - Ano: {this.ano} - Modelo: {this.modelo}\nNumero de Portas: {this.numPortas} - Tipo de Combustível: {this.tipoCombustivel}");
+        System.Console.WriteLine($"Carro - Modelo: {this.modelo} - Ano: {this.ano} - Modelo: {this.modelo}\nNumero de Portas: {this.numPortas} - Tipo de Combustível: {this.tipoCombustivel}");
     }
     public override double CalcularCustoManutencao()
     {
@@ -36,7 +36,7 @@ class Moto : Veiculo
     public string tipoPartida;
     public override void ExibirDetalhes()
     {
-        System.Console.WriteLine($"Modelo: {this.modelo} - Ano: {this.ano} - Modelo: {this.modelo}\nCilindadas: {this.cilindrada} - Tipo de Partida: {this.tipoPartida}");
+        System.Console.WriteLine($"Moto - Modelo: {this.modelo} - Ano: {this.ano} - Modelo: {this.modelo}\nCilindadas: {this.cilindrada} - Tipo de Partida: {this.tipoPartida}");
     }
     public override double CalcularCustoManutencao()
     {
@@ -54,7 +54,7 @@ class Caminhao : Veiculo
     public int numEixos { get; set; }
     public override void ExibirDetalhes()
     {
-        System.Console.WriteLine($"Modelo: {this.modelo} - Ano: {this.ano} - Modelo: {this.modelo}\nCapacidade de carga: {this.capacidadeCarga} - Número de Eixos: {this.numEixos}");
+        System.Console.WriteLine($"Caminhão - Modelo: {this.modelo} - Ano: {this.ano} - Modelo: {this.modelo}\nCapacidade de carga: {this.capacidadeCarga} - Número de Eixos: {this.numEixos}");
     }
     public override double CalcularCustoManutencao()
     {
@@ -68,6 +68,7 @@ class Caminhao : Veiculo
 }
 class FabricaVeiculo
 {
+    public List<Veiculo> listaVeiculos = new List<Veiculo>();
     public Veiculo CriarVeiculo(string tipo)
     {
         Veiculo v;
@@ -80,7 +81,6 @@ class FabricaVeiculo
         cor = Console.ReadLine();
         System.Console.WriteLine("Qual ano do veiculo?");
         ano = int.Parse(Console.ReadLine());
-
         if (tipo == "carro")
         {
             System.Console.WriteLine("Qual tipo de Combustivel?");
@@ -88,6 +88,7 @@ class FabricaVeiculo
             System.Console.WriteLine("Qual numero de portas?");
             numPortas = int.Parse(Console.ReadLine());
             v = new Carro(modelo, cor, ano, numPortas, tipoCombustivel);
+            listaVeiculos.Add(v);
             return v;
         }
         else if (tipo == "moto")
@@ -97,6 +98,7 @@ class FabricaVeiculo
             System.Console.WriteLine("Qual cilindrada?");
             cilindrada = int.Parse(Console.ReadLine());
             v = new Moto(modelo, cor, ano, cilindrada, tipoPartida);
+            listaVeiculos.Add(v);
             return v;
         }
         else
@@ -106,20 +108,34 @@ class FabricaVeiculo
             System.Console.WriteLine("Qual capacidade de caga?");
             capacidadeCarga = double.Parse(Console.ReadLine());
             v = new Caminhao(modelo, cor, ano, capacidadeCarga, numEixos);
+            listaVeiculos.Add(v);
             return v;
+        }
+    }
+    public void ListaVeiculos()
+    {
+        foreach (Veiculo v in listaVeiculos)
+        {
+            v.ExibirDetalhes();
         }
     }
 }
 class Program
 {
+    static void Menu()
+    {
+        System.Console.WriteLine($"1) Adicionar Veículos \n2) Listar Veículos\n0) Encerrar Programa");
+    }
     static void Main()
     {
         int op = 1000;
-        string tipo;
+        string tipo = "";
+        double total = 0;
+        FabricaVeiculo fabrica = new FabricaVeiculo();
         do
         {
             System.Console.WriteLine("Selecione uma opção:");
-            System.Console.WriteLine("");
+            Menu();
             op = int.Parse(Console.ReadLine());
             switch (op)
             {
@@ -127,19 +143,26 @@ class Program
                     System.Console.WriteLine("Encerrando o programa...");
                     break;
                 case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
+                    System.Console.WriteLine("Qual tipo de veículo deseja criar? (carro, moto, caminhao)");
+                    tipo = Console.ReadLine();
+                    while (tipo != "carro" && tipo != "moto" && tipo != "caminhao")
+                    {
+                        System.Console.WriteLine("Insira um modelo válido");
+                        tipo = Console.ReadLine();
+                    }
+                    fabrica.CriarVeiculo(tipo);
+                    tipo = "";
                     break;
                 default:
                     System.Console.WriteLine("Insira uma opção válida!");
                     break;
             }
         } while (op != 0);
+        foreach (Veiculo v in fabrica.listaVeiculos)
+        {
+            v.ExibirDetalhes();
+            total += v.CalcularCustoManutencao();
+        }
+        System.Console.WriteLine($"Custo total de manutenção: {total}");
     }
 }
